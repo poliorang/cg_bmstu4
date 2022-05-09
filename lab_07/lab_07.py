3ef57e46fb295e68e6db1fbc84273e862630b7b6
poliorang <poliorang05@mail.ru>
2022-05-10 01:42:06 +0300

    Done lab_07

--- lab_07/lab_07.py
+++ lab_07/lab_07.py
@@ -1,20 +1,16 @@
 from tkinter import messagebox, ttk, colorchooser, PhotoImage
 from tkinter import *
 import colorutils as cu
-from time import time, sleep
-import matplotlib.path as mplPath
-import numpy as np
 
 WIN_WIDTH = 1200
 WIN_HEIGHT = 800
-TEMP_SIDE_COLOR_CHECK = (255, 0, 255)  # purple
-TEMP_SIDE_COLOR = "#ff00ff"
 
 SIZE = 800
 WIDTH = 100.0
 
-TASK = "Реализация и исследование " \
-       "алгоритма построчного затравочного заполнения."
+TASK = "Реализация (и исследование) " \
+       "отсечения отрезка регулярным отсекателем " \
+       "методом Сазерленда-Коэна"
 
 AUTHOR = "\n\nЕгорова Полина ИУ7-44Б"
 
@@ -38,8 +34,17 @@ def to_canva(dot):
 # нарисовать отрезок
 def draw_line():
     color = cu.Color(line_color[1])
-    canvas_win.create_line(line_coords[0], line_coords[1], fill=color, tag='line')
-    print(lines)
+    try:
+        dot1 = to_canva([int(x1_entry.get()), int(y1_entry.get())])
+        dot2 = to_canva([int(x2_entry.get()), int(y2_entry.get())])
+    except ValueError:
+        messagebox.showerror("Ошибка", "Некорректные координаты отрезка")
+        return
+
+    lines.append([dot1, dot2])
+    history.append([dot1, dot2, 'line'])
+
+    canvas_win.create_line(dot1, dot2, fill=color, tag='line')
 
 
 # нарисовать отсекатель
@@ -48,7 +53,13 @@ def draw_clipper():
     canvas_win.delete('clipper')
     color = cu.Color(clipper_color[1])
 
-    canvas_win.create_rectangle(clipper_coords[0], clipper_coords[1], outline=color, tag='clipper')
+    dot1 = to_canva([int(x1_clipper_entry.get()), int(y1_clipper_entry.get())])
+    dot2 = to_canva([int(x2_clipper_entry.get()), int(y2_clipper_entry.get())])
+
+    clipper_coords = [dot1, dot2]
+    history.append([dot1, dot2, 'rectangle'])
+
+    canvas_win.create_rectangle(dot1, dot2, outline=color, tag='clipper')
 
 
 # отрисовка и вставка в листбокс добавленной точки
@@ -63,10 +74,6 @@ def draw_point(ev_x, ev_y, click_):
     x_y = to_coords([x, y])
 
     if option_line.get() == 0:
-        if len(line_coords) > 0:
-            line_coords = []
-        line_coords.append([x, y])
-
         x1_entry.delete(0, END)
         y1_entry.delete(0, END)
         x1_entry.insert(0, "%d" % x_y[0])
@@ -75,11 +82,6 @@ def draw_point(ev_x, ev_y, click_):
         canvas_win.create_oval(x - 2, y - 2, x + 2, y + 2,
                                outline='lightgreen', fill='lightgreen', activeoutline='pink', width=2, tag='dot1')
     elif option_line.get() == 1:
-        if len(line_coords) == 2:
-            line_coords = line_coords[0:1]
-        line_coords.append([x, y])
-        lines.append(line_coords)
-
         x2_entry.delete(0, END)
         y2_entry.delete(0, END)
         x2_entry.insert(0, "%d" % x_y[0])
@@ -89,11 +91,6 @@ def draw_point(ev_x, ev_y, click_):
                                outline='lightgreen', fill='lightgreen', activeoutline='pink', width=2, tag='dot2')
 
     elif option_line.get() == 2:
-        if len(clipper_coords) > 0:
-            clipper_coords[0] = [x, y]
-        else:
-            clipper_coords.append([x, y])
-
         x1_clipper_entry.delete(0, END)
         y1_clipper_entry.delete(0, END)
         x1_clipper_entry.insert(0, "%d" % x_y[0])
@@ -102,10 +99,6 @@ def draw_point(ev_x, ev_y, click_):
         canvas_win.create_oval(x - 2, y - 2, x + 2, y + 2,
                                outline='pink', fill='pink', activeoutline='lightgreen', width=2, tag='clipper1')
     elif option_line.get() == 3:
-        if len(clipper_coords) == 2:
-            clipper_coords = clipper_coords[0:1]
-        clipper_coords.append([x, y])
-
         x2_clipper_entry.delete(0, END)
         y2_clipper_entry.delete(0, END)
         x2_clipper_entry.insert(0, "%d" % x_y[0])
@@ -117,16 +110,15 @@ def draw_point(ev_x, ev_y, click_):
 
 def get_dot_bits(clipper, dot):
     bits = 0b0000
-    # print('clipper ', clipper, '\ndot ', dot)
 
     if dot[0] < clipper[0]:
         bits += 0b0001
 
-    if dot[1] < clipper[1]:
-        bits += 0b0100
-
-    if dot[0] > clipper[2]:
+    if dot[0] > clipper[1]:
         bits += 0b0010
+
+    if dot[1] < clipper[2]:
+        bits += 0b0100
 
     if dot[1] > clipper[3]:
         bits += 0b1000
@@ -137,7 +129,6 @@ def get_dot_bits(clipper, dot):
 def check_visible(dot1_bits, dot2_bits):
     vision = 0  # частично видимый
 
-    # print(dot1_bits, dot2_bits)
     if dot1_bits == 0 and dot2_bits == 0:
         vision = 1  # видим
     elif dot1_bits & dot2_bits:
@@ -171,7 +162,7 @@ def method_sazerland_kohen(clipper, line):
         if m == 0:
             fl = 1  # горизонтальный
 
-    for i in (0, 2, 1, 3):
+    for i in range(4):
         dot1_bits = get_dot_bits(clipper, dot1)
         dot2_bits = get_dot_bits(clipper, dot2)
 
@@ -208,14 +199,19 @@ def method_sazerland_kohen(clipper, line):
 def cut_area():
     global clipper_coords
 
-    if len(clipper_coords) != 2:
+    print(clipper_coords)
+    if len(clipper_coords) < 1:
         messagebox.showinfo("Ошибка", "Не задан отсекатель")
         return
 
-    clipper = [min(clipper_coords[0][0], clipper_coords[1][0]), min(clipper_coords[0][1], clipper_coords[1][1]),
-               max(clipper_coords[0][0], clipper_coords[1][0]), max(clipper_coords[0][1], clipper_coords[1][1])]
+    if len(lines) < 1:
+        messagebox.showinfo("Ошибка", "Не задан ни один отрезок")
+        return
+
+    clipper = [min(clipper_coords[0][0], clipper_coords[1][0]), max(clipper_coords[0][0], clipper_coords[1][0]),
+               min(clipper_coords[0][1], clipper_coords[1][1]), max(clipper_coords[0][1], clipper_coords[1][1])]
+
     for line in lines:
-        print(line)
         method_sazerland_kohen(clipper, line)
 
 
@@ -238,35 +234,21 @@ def del_dot():
         messagebox.showerror("Ошибка", "Не выбрана точка")
 
 
-# откат
-def undo():
-    global dots_list
-
-    if len(dots_list) == 1 and dots_list[0] == []:
-        messagebox.showerror("Внимание", "Достигнуто исходное состояние")
-        return
-
-    canvas_win.delete('line', 'coord')
-
-    d = -1
-    if not dots_list[-1]:
-        if len(dots_list) > 1:
-            d = -2
-    dots_list[d].pop()
-    del_dot()
-
-    if len(dots_list) > 1 and dots_list[-2] == []:
-        dots_list = dots_list[:-1]
-
-    draw_lines(dots_list)
-    draw_axes()
+def draw_all():
+    color_line = cu.Color(line_color[1])
+    color_clipper = cu.Color(clipper_color[1])
+    for figure in history:
+        if figure[2] == 'line':
+            canvas_win.create_line(figure[0], figure[1], fill=color_line, tag='line')
+        elif figure[2] == 'rectangle':
+            canvas_win.create_rectangle(figure[0], figure[1], outline=color_clipper, tag='clipper')
 
 
 # изменение цвета фона
 def change_bg_color():
+    global canvas_color
     canvas_color = colorchooser.askcolor()
     canvas_win.configure(bg=cu.Color(canvas_color[1]))
-    parse_fill()
 
 
 # изменение цвета отрезка, результата или отсекателя
@@ -313,13 +295,47 @@ def draw_with_enter(event):
         draw_clipper()
 
 
+# определить крайний отсекатель для ундо
+def find_rectangle(history):
+    for i in range(len(history) - 1, -1, -1):
+        if history[i][2] == 'rectangle':
+            return history[0:2]
+
+    return []
+
+
+# откат
+def undo():
+    global history, clipper_coords, lines
+
+    print(*history)
+    if len(history) == 0:
+        messagebox.showerror("Внимание", "Достигнуто исходное состояние")
+        return
+
+    canvas_win.delete('line', 'dot1', 'dot2', 'clipper1', 'clipper2', 'clipper', 'result')
+
+    deleted = history.pop()
+    if deleted[2] == 'line':
+        lines.pop()
+    elif deleted[2] == 'rectangle':
+        clipper_coords = find_rectangle(history)
+
+    draw_all()
+
+
 #  отчистака канваса
 def clean_canvas():
-    global canvas_color
+    global canvas_color, history, lines, clipper_coords
+
+    history = []
+    lines = []
+    clipper_coords = []
 
     canvas_win.delete('line', 'dot1', 'dot2', 'clipper1', 'clipper2', 'clipper', 'result')
     canvas_color = ((255, 255, 255), "#ffffff")
     canvas_win.configure(bg=cu.Color(canvas_color[1]))
+
 
 
 # оси координат и сетка
@@ -333,15 +349,11 @@ def draw_axes():
                            width=1, arrow=LAST, activefill='lightgreen', arrowshape="10 20 6")
 
     for i in range(0, s, s // 16):
-        # canvas_win.create_line(i, s / 2 - 5, i, s / 2 + 5, fill='grey',
-        #                        width=1, arrow=LAST, activefill='lightgreen', arrowshape="10 20 6")
         canvas_win.create_line(i, s / 2 - 5, i, s / 2 + 5, fill='pink', width=2)
         canvas_win.create_line(i, 0, i, s, fill='grey', width=1, dash=(1, 9))
         canvas_win.create_text(i, s // 2 + 20, text=f'{"%.2f" % xy_current[j]}' if i - SIZE // 2 else '',
                                fill='grey', tag='coord', font="AvantGardeC 10")
 
-        # canvas_win.create_line(s / 2 - 5, i, s / 2 + 5, i, fill='grey',
-        #                        width=1, arrow=LAST, activefill='lightgreen', arrowshape="10 20 6")
         canvas_win.create_line(s / 2 - 5, i, s / 2 + 5, i, fill='pink', width=2)
         canvas_win.create_line(0, i, s, i, fill='grey', width=1, dash=(1, 9))
         canvas_win.create_text(s // 2 - 20, i, text=f'{"%.2f" % xy_current[16 - j]}' if i - SIZE // 2 else '',
@@ -369,64 +381,65 @@ def config(event):
         canvas_win.place(x=300 * win_x, y=0 * win_y, width=size, height=size)
         canvas_win.create_image((WIN_WIDTH / 2, WIN_HEIGHT / 2), image=image_canvas, state="normal")
 
+        info_lbl.place(x=30 * win_x, y=20 * win_y, width=237 * win_x, height=65 * win_y)
 
         # координаты отрезка
-        line_lbl.place(x=30 * win_x, y=48 * win_y, width=237 * win_x, height=24 * win_y)
+        line_lbl.place(x=30 * win_x, y=108 * win_y, width=237 * win_x, height=24 * win_y)
 
-        x1_lbl.place(x=30 * win_x, y=80 * win_y, width=30 * win_x, height=18 * win_y)
-        y1_lbl.place(x=156 * win_x, y=80 * win_y, width=30 * win_x, height=18 * win_y)
-        x1_entry.place(x=62 * win_x, y=80 * win_y, width=80 * win_x, height=20 * win_y)
-        y1_entry.place(x=188 * win_x, y=80 * win_y, width=80 * win_x, height=20 * win_y)
+        x1_lbl.place(x=30 * win_x, y=140 * win_y, width=30 * win_x, height=18 * win_y)
+        y1_lbl.place(x=156 * win_x, y=140 * win_y, width=30 * win_x, height=18 * win_y)
+        x1_entry.place(x=62 * win_x, y=140 * win_y, width=80 * win_x, height=20 * win_y)
+        y1_entry.place(x=188 * win_x, y=140 * win_y, width=80 * win_x, height=20 * win_y)
 
-        x2_lbl.place(x=30 * win_x, y=102 * win_y, width=30 * win_x, height=18 * win_y)
-        y2_lbl.place(x=156 * win_x, y=102 * win_y, width=30 * win_x, height=18 * win_y)
-        x2_entry.place(x=62 * win_x, y=102 * win_y, width=80 * win_x, height=20 * win_y)
-        y2_entry.place(x=188 * win_x, y=102 * win_y, width=80 * win_x, height=20 * win_y)
+        x2_lbl.place(x=30 * win_x, y=162 * win_y, width=30 * win_x, height=18 * win_y)
+        y2_lbl.place(x=156 * win_x, y=162 * win_y, width=30 * win_x, height=18 * win_y)
+        x2_entry.place(x=62 * win_x, y=162 * win_y, width=80 * win_x, height=20 * win_y)
+        y2_entry.place(x=188 * win_x, y=162 * win_y, width=80 * win_x, height=20 * win_y)
 
-        add_line.place(x=30 * win_x, y=125 * win_y, width=237 * win_x, height=25 * win_y)
+        add_line.place(x=30 * win_x, y=185 * win_y, width=237 * win_x, height=25 * win_y)
 
-        point1_radio.place(x=3 * win_x, y=75 * win_y)
-        point2_radio.place(x=3 * win_x, y=97 * win_y)
+        point1_radio.place(x=3 * win_x, y=135 * win_y)
+        point2_radio.place(x=3 * win_x, y=157 * win_y)
 
 
         # координаты отсекателя
-        clipper_lbl.place(x=30 * win_x, y=190 * win_y, width=196 * win_x, height=24 * win_y)
+        clipper_lbl.place(x=30 * win_x, y=250 * win_y, width=196 * win_x, height=24 * win_y)
 
-        x1_clipper_lbl.place(x=30 * win_x, y=222 * win_y, width=30 * win_x, height=18 * win_y)
-        y1_clipper_lbl.place(x=156 * win_x, y=222 * win_y, width=30 * win_x, height=18 * win_y)
-        x1_clipper_entry.place(x=62 * win_x, y=222 * win_y, width=80 * win_x, height=20 * win_y)
-        y1_clipper_entry.place(x=188 * win_x, y=222 * win_y, width=80 * win_x, height=20 * win_y)
+        x1_clipper_lbl.place(x=30 * win_x, y=282 * win_y, width=30 * win_x, height=18 * win_y)
+        y1_clipper_lbl.place(x=156 * win_x, y=282 * win_y, width=30 * win_x, height=18 * win_y)
+        x1_clipper_entry.place(x=62 * win_x, y=282 * win_y, width=80 * win_x, height=20 * win_y)
+        y1_clipper_entry.place(x=188 * win_x, y=282 * win_y, width=80 * win_x, height=20 * win_y)
 
-        x2_clipper_lbl.place(x=30 * win_x, y=244 * win_y, width=30 * win_x, height=18 * win_y)
-        y2_clipper_lbl.place(x=156 * win_x, y=244 * win_y, width=30 * win_x, height=18 * win_y)
-        x2_clipper_entry.place(x=62 * win_x, y=244 * win_y, width=80 * win_x, height=20 * win_y)
-        y2_clipper_entry.place(x=188 * win_x, y=244 * win_y, width=80 * win_x, height=20 * win_y)
+        x2_clipper_lbl.place(x=30 * win_x, y=304 * win_y, width=30 * win_x, height=18 * win_y)
+        y2_clipper_lbl.place(x=156 * win_x, y=304 * win_y, width=30 * win_x, height=18 * win_y)
+        x2_clipper_entry.place(x=62 * win_x, y=304 * win_y, width=80 * win_x, height=20 * win_y)
+        y2_clipper_entry.place(x=188 * win_x, y=304 * win_y, width=80 * win_x, height=20 * win_y)
 
-        add_clipper.place(x=30 * win_x, y=267 * win_y, width=237 * win_x, height=25 * win_y)
+        add_clipper.place(x=30 * win_x, y=327 * win_y, width=237 * win_x, height=25 * win_y)
 
-        clipper1_radio.place(x=3 * win_x, y=217 * win_y)
-        clipper2_radio.place(x=3 * win_x, y=239 * win_y)
+        clipper1_radio.place(x=3 * win_x, y=277 * win_y)
+        clipper2_radio.place(x=3 * win_x, y=299 * win_y)
 
-        info_clipper.place(x=228 * win_x, y=190 * win_y, width=40 * win_x, height=24 * win_y)
+        info_clipper.place(x=228 * win_x, y=250 * win_y, width=40 * win_x, height=24 * win_y)
 
 
         # цвет фона, отсекателя, отрезка и результата
-        color_lbl.place(x=30 * win_x, y=400 * win_y, width=237 * win_x, height=24 * win_y)
+        color_lbl.place(x=30 * win_x, y=390 * win_y, width=237 * win_x, height=24 * win_y)
 
-        bg_clr.place(x=30 * win_x, y=427 * win_y, width=111 * win_x, height=25 * win_y)
-        clipper_clr.place(x=155 * win_x, y=427 * win_y, width=111 * win_x, height=25 * win_y)
-        line_clr.place(x=30 * win_x, y=453 * win_y, width=111 * win_x, height=25 * win_y)
-        result_clr.place(x=155 * win_x, y=453 * win_y, width=111 * win_x, height=25 * win_y)
-
+        bg_clr.place(x=30 * win_x, y=417 * win_y, width=111 * win_x, height=25 * win_y)
+        clipper_clr.place(x=155 * win_x, y=417 * win_y, width=111 * win_x, height=25 * win_y)
+        line_clr.place(x=30 * win_x, y=443 * win_y, width=111 * win_x, height=25 * win_y)
+        result_clr.place(x=155 * win_x, y=443 * win_y, width=111 * win_x, height=25 * win_y)
 
         # отсечь
-        bld.place(x=30 * win_x, y=560 * win_y, width=235 * win_x, height=28 * win_y)
+        bld.place(x=30 * win_x, y=500 * win_y, width=235 * win_x, height=32 * win_y)
+
         # условие
-        con.place(x=30 * win_x, y=620 * win_y, width=235 * win_x, height=28 * win_y)
+        con.place(x=30 * win_x, y=650 * win_y, width=235 * win_x, height=28 * win_y)
         # откат
-        und.place(x=30 * win_x, y=650 * win_y, width=109 * win_x, height=28 * win_y)
+        und.place(x=30 * win_x, y=680 * win_y, width=109 * win_x, height=28 * win_y)
         # к начальным условиям
-        bgn.place(x=157 * win_x, y=650 * win_y, width=109 * win_x, height=28 * win_y)
+        bgn.place(x=157 * win_x, y=680 * win_y, width=109 * win_x, height=28 * win_y)
 
 
         canvas_win.delete('all')
@@ -471,6 +484,13 @@ win.bind("<Return>", draw_with_enter)
 option_line = IntVar()
 option_line.set(0)
 
+
+# информация о клавишах
+INFO = "w - движение вверх по радиобаттону\n" \
+       "s - движение вниз по радиобаттону\n" \
+       "Enter - построить отрезок\n" \
+       "        или отсекатель"
+info_lbl = Label(text=INFO, font="AvantGardeC 12", fg='grey', bg='lightgrey')
 
 # отрезок
 line_lbl = Label(text="Координаты отрезка", bg='pink', font="AvantGardeC 14", fg='black')
@@ -528,7 +548,10 @@ info_clipper = Button(text="?", font="AvantGardeC 14",
 
 line_coords = []
 clipper_coords = []
+history = []
+
 lines = []
+clippers = []
 
 
 # Кнопки
